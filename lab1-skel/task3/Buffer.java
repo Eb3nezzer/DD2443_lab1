afecc177b9bf9e84cbdd2066aadc152945a76022
Max Harrison <u7130836@anu.edu.au>
2025-09-02 19:25:30 +0200

    Exercise 3 mostly done

--- lab1-skel/task3/Buffer.java
+++ lab1-skel/task3/Buffer.java
@@ -1,19 +1,94 @@
+import java.util.concurrent.locks.Condition;
+import java.util.concurrent.locks.Lock;
+import java.util.concurrent.locks.ReentrantLock;
+
 public class Buffer {
+	final Lock lock = new ReentrantLock();
+	final java.util.concurrent.locks.Condition notFull = lock.newCondition();
+	final Condition notEmpty = lock.newCondition();
+
+	final int[] ints;
+	int add_index = 0, remove_index = 0;
+	int count = 0;
+	final int capacity;
+
+	boolean closed = false;
 
 	public Buffer(int size) {
-		// TODO
+		ints = new int[size];
+		capacity = size;
 	}
 
-	void add(int i) {
-		// TODO
+	void add(int i) throws InterruptedException {
+		// claim the lock
+		lock.lock();
+		try {
+			// check that there are spaces available, if not wait on condition
+			while (count == capacity && !closed) {
+				notFull.await();
+			}
+
+			// check that the buffer is not closed
+			if (closed) {
+				throw new IllegalStateException("Buffer is closed");
+			}
+
+			// add item to buffer
+			ints[add_index] = i;
+			add_index = (add_index + 1) & capacity;
+			count++;
+
+			// signal that buffer is not empty
+			notEmpty.signal();
+		} finally {
+			// release the lock
+			lock.unlock();
+		}
 	}
 
-	public int remove() {
-		// TODO
-		return -1;
+	public int remove() throws InterruptedException {
+		// claim the lock
+		lock.lock();
+		try {
+			// check that there are items available, if not wait on condition
+			while (count == 0 && !closed) {
+				notEmpty.await();
+			}
+
+			// check if buffer is closed and empty
+			if (closed && count == 0) {
+				throw new IllegalStateException("Buffer is closed and empty");
+			}
+
+			// take item from buffer
+			int obj = ints[remove_index];
+			remove_index = (remove_index + 1) % capacity;
+			count--;
+
+			// signal that buffer is not full
+			notFull.signal();
+
+			return obj;
+		} finally {
+			// release the lock
+			lock.unlock();
+		}
 	}
 
 	public void close() {
-		// TODO
+		// claim the lock
+		lock.lock();
+		try {
+			// check that the buffer is not already closed
+			if (closed) {
+				throw new IllegalStateException("Buffer already closed");
+			} else {
+				closed = true;
+			}
+		} finally {
+			// release the lock
+			lock.unlock();
+		}
+
 	}
 }
--- lab1-skel/task3/Main.java
+++ lab1-skel/task3/Main.java
@@ -1,14 +1,55 @@
 public class Main {
+	private static int buffer_size = 1_000_000;
+	private static volatile Buffer buffer = new Buffer(buffer_size);
+
 	public static class Producer implements Runnable {
 		public void run() {
+			for (int i = 0; i<1_000_000; i++) {
+				try {
+					buffer.add(i);
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+			buffer.close();
 		}
 	}
 
 	public static class Consumer implements Runnable {
 		public void run() {
+			try {
+				while (true) {
+					try {
+						int value = buffer.remove();
+						System.out.println(value);
+					} catch (IllegalStateException e) {
+						// Buffer is closed and empty - exit normally
+						break;
+					}
+				}
+			} catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+			System.out.println("Buffer is now closed, exiting");
 		}
 	}
 
-	public static void main(String [] args) {
+	public static void main(String [] args) throws InterruptedException  {
+		int n = 2; // number of threads
+        Thread[] threads = new Thread[n];
+
+        // Create threads
+		threads[0] = new Thread(new Producer());
+		threads[1] = new Thread(new Consumer());
+
+        // Start threads
+        for (Thread t : threads) {
+            t.start();
+        }
+
+        // Wait for all to finish
+        for (Thread t : threads) {
+            t.join();
+        }
 	}
 }
